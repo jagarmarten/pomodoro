4a32c256d197235d5e1749ca97a359950cff46ff
Martin Kozon <martin.kozon12@gmail.com>
2017-10-06 23:05:33 +0200

    buttons

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*next thing I need to do is:
-    1. configure startTimer(), pauseTimer() and resetTimer() functions
+    1. configure startTimer(), pauseTimer() and resetTimer() functions - 90% DONE, need to repair the Play because if you click the play twice the timer will go 2 times faster
     2. when break is clicked, chenge the time to 5 minutes; when pomodoro is clicked, change the time to 25
         - maybe via tabs or just simple js code
 */
@@ -7,14 +7,13 @@
 var minutes = 25;   //set time in minutes
 
 var count = minutes * 60; //multiply minutes by 60 to get seconds
+var countReset = count;
 
-var counter=setInterval(timer, 1000); //counter will update every 1 second (1000 miliseconds)
+var counter;
 
-function timer() {
+function printOut() {
     var minutes = Math.floor(count / 60); //get minutes left
     var seconds = count % 60; //get how many seconds are left with modulus (%)
-
-    count--; //decrement by 1
 
     //if seconds are less then 10, add 0 and then the number left
     //example: 0:9 seconds are left; add 0 in front of 9; we'll get 0:09 seconds left
@@ -22,12 +21,36 @@ function timer() {
         seconds = "0" + seconds;
     }
 
-    //if count is less then 0, stop the interval
-    if (count < 0) {
-         clearInterval(counter);
-    }
+    //print out to HTML document - minutes:seconds
+    document.getElementById("timer").innerHTML = minutes + ":" +seconds;
+}
 
-    //print out to HTML document
-    document.getElementById("timer").innerHTML = minutes + ":" +seconds; // watch for spelling
-    //Do code for showing the number of seconds here
+function timer() {
+
+    printOut();
+    
+    if (count == 0) {
+        //if count is 0, stop the counter
+        clearTimeout(counter);
+        console.log("stop");
+    } else {
+        //else - continue the timer
+        count--;
+        counter = setTimeout(timer, 1000)
+        console.log("continue");
+    }
+}
+
+function pauseTimer() {
+    //pause the timer by clearing the Timeout
+    clearTimeout(counter);
+    console.log("pause");
+}
+
+function resetTimer() {
+    //reset the timer by clearing the timeout, then changing the number value to the default value and printing out the default value
+    clearTimeout(counter);
+    count = countReset;
+    printOut();
+    console.log("reset")
 }

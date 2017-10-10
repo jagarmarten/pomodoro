30560d51fbf53c92c18f04d238ca7198eb59ff59
Martin Kozon <martin.kozon12@gmail.com>
2017-10-10 20:04:19 +0200

    updates

--- app.js
+++ app.js
@@ -1,56 +1,100 @@
-/*next thing I need to do is:
-    1. configure startTimer(), pauseTimer() and resetTimer() functions - 90% DONE, need to repair the Play because if you click the play twice the timer will go 2 times faster
-    2. when break is clicked, chenge the time to 5 minutes; when pomodoro is clicked, change the time to 25
-        - maybe via tabs or just simple js code
-*/
+//start javascript only after the DOM's loaded
+$(document).ready(function() {
 
-var minutes = 25;   //set time in minutes
-
-var count = minutes * 60; //multiply minutes by 60 to get seconds
-var countReset = count;
-
-var counter;
-
-function printOut() {
-    var minutes = Math.floor(count / 60); //get minutes left
-    var seconds = count % 60; //get how many seconds are left with modulus (%)
-
-    //if seconds are less then 10, add 0 and then the number left
-    //example: 0:9 seconds are left; add 0 in front of 9; we'll get 0:09 seconds left
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-
-    //print out to HTML document - minutes:seconds
-    document.getElementById("timer").innerHTML = minutes + ":" +seconds;
-}
-
-function timer() {
-
-    printOut();
+    var minutes = 25;   //set time in minutes
+    var count = minutes * 60; //multiply minutes by 60 to get seconds
+    var countReset = count;
     
-    if (count == 0) {
-        //if count is 0, stop the counter
-        clearTimeout(counter);
-        console.log("stop");
-    } else {
-        //else - continue the timer
-        count--;
-        counter = setTimeout(timer, 1000)
-        console.log("continue");
+    var counter; //counter variable I'm using later
+    var do_something = false;
+    
+    var play = $("#play"); //play button
+    var pause = $("#pause"); //pause button
+    var reset = $("#reset"); //reset button
+    
+    function printOut() {
+        var minutes = Math.floor(count / 60); //get minutes left
+        var seconds = count % 60; //get how many seconds are left with modulus (%)
+    
+        //if seconds are less then 10, add 0 and then the number left
+        //example: 0:9 seconds are left; add 0 in front of 9; we'll get 0:09 seconds left
+        if (seconds < 10) {
+            seconds = "0" + seconds; //it won't be 9 but 09 seconds left
+        }
+    
+        //print out to HTML document - minutes:seconds
+        $("#timer").text(minutes + ":" + seconds);
+        //$("#timer").innerHTML = minutes + ":" +seconds;
     }
-}
 
-function pauseTimer() {
-    //pause the timer by clearing the Timeout
-    clearTimeout(counter);
-    console.log("pause");
-}
+    //print out the time left to <p id="timer"></p>
+    $("#timer").text(printOut());
 
-function resetTimer() {
-    //reset the timer by clearing the timeout, then changing the number value to the default value and printing out the default value
-    clearTimeout(counter);
-    count = countReset;
-    printOut();
-    console.log("reset")
-}
+    //when play button is clicked, do this
+    play.click( function timer() {
+    
+        clearTimeout(counter); //clear the timeout first
+        printOut(); //calling for printOut(); function
+
+        //.active class adds some fancy styles to the button
+        play.addClass("active"); //add class active to play button
+        
+        //remove class .active from other elements if they have it
+        if(pause.hasClass("active") || reset.hasClass("active")) {
+            pause.removeClass("active"); //remove class .active
+            reset.removeClass("active"); //remove class .active
+        }
+        
+        if (count == 0) {
+            //if count is 0, stop the counter
+            clearTimeout(counter); //clear the timeout
+            console.log("stop"); //console.log the event
+        } else {
+            //else - continue the timer
+            if(!do_something) {
+                count--; //decrement by 1
+                counter = setTimeout(timer, 1000); //set timeout to 1 second
+                console.log("continue"); //console.log the event
+            } else {
+                click_handler();
+            }
+        }
+    });
+    
+    function click_handler() {
+        do_something = true; //this checks if the function is already running, if true you won'r be able to click the button play twice
+    }
+    
+    //when pause button is clicked, do this
+    pause.click( function pauseTimer() {
+        //.active class adds some fancy styles to the button
+        pause.addClass("active");  //add class active to pause button
+        
+        //remove class .active from other elements if they have it
+        if(play.hasClass("active") || reset.hasClass("active")) {
+            play.removeClass("active"); //remove class .active
+            reset.removeClass("active"); //remove class .active
+        }
+    
+        clearTimeout(counter); //pause the timer by clearing the Timeout
+        console.log("pause"); //console.log the event
+    });
+    
+    //when reset button is clicked, do this
+    reset.click( function resetTimer() {
+        //.active class adds some fancy styles to the button
+        reset.addClass("active");  //add class active to reset button
+
+        //remove class .active from other elements if they have it
+        if(play.hasClass("active") || pause.hasClass("active")) {
+            play.removeClass("active"); //remove class .active
+            pause.removeClass("active"); //remove class .active
+        }
+
+        //reset the timer by clearing the timeout, then changing the number value to the default value and printing out the default value
+        clearTimeout(counter); //clear timeout
+        count = countReset;
+        printOut(); 
+        console.log("reset"); //console.log the event
+    });
+});
